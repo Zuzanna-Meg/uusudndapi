5f44e0c03b5b9b8183f53608dfab4e9d8c999397
Zuzanna <zmegger891@gmail.com>
2024-01-06 22:04:02 +0000

    Initial commit

---
+++ src/main/java/com668/uusudnd/api/uusudndapi/UusudndapiApplication.java
@@ -0,0 +1,13 @@
+package com668.uusudnd.api.uusudndapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UusudndapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UusudndapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com668/uusudnd/api/uusudndapi/UusudndapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com668.uusudnd.api.uusudndapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UusudndapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
